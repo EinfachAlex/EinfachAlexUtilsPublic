43d0f8fcbe1c7138878708f71b814b8bca76deb6
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2022-02-13 23:55:45 +0100

    add LoggerCommonMessages.cs

---
+++ Logging/LoggerCommonMessages.cs
@@ -0,0 +1,11 @@
+﻿using EinfachAlex.Utils.WebRequest;
+
+namespace EinfachAlex.Utils.Logging
+{
+    public class LoggerCommonMessages
+    {
+        public static string PREFIX_WEBREQUEST = "[WEBREQUEST] ";
+        
+        public static void logEndpointRequest(string endpoint, ERequestTypes type) => Logger.s(PREFIX_WEBREQUEST + $"{type.ToString()} {endpoint}");
+    }
+}
