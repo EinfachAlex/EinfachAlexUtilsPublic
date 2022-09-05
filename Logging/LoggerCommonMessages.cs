a411c89f2f216d044dbd6098e3e9b97ec33dc2e9
alexander <einfachalex@sags-per-mail.de>
2022-09-05 23:54:58 +0200

    add new logging methods

--- Logging/LoggerCommonMessages.cs
+++ Logging/LoggerCommonMessages.cs
@@ -1,4 +1,5 @@
-﻿using EinfachAlex.Utils.WebRequest;
+﻿using EinfachAlex.Utils.HashGenerator;
+using EinfachAlex.Utils.WebRequest;
 
 namespace EinfachAlex.Utils.Logging
 {
@@ -7,5 +8,18 @@ namespace EinfachAlex.Utils.Logging
         public static string PREFIX_WEBREQUEST = "[WEBREQUEST] ";
         
         public static void logEndpointRequest(string endpoint, ERequestTypes type) => Logger.s(PREFIX_WEBREQUEST + $"{type.ToString()} {endpoint}");
+       
+        public static void logConstructNew(string key)
+        {
+            //Get what type is constructed based on call stack (name of calling class)
+            string constructedItem = (new System.Diagnostics.StackTrace()).GetFrame(1).GetMethod().DeclaringType?.Name;
+            
+            Logger.i($"Trying to construct new {constructedItem} {key}");
+        }
+        
+        public static void logGeneratedId(Hash id)
+        {
+            Logger.v($"Generated ID {id}");
+        }
     }
 }
